00b6f630f9cc82a1ad187a0072f553f548471319
Benjamin <brulaire@keensaas.fr>
2018-11-08 17:25:53 +0100

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   before_save { self.email = email.downcase }
 
-    validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
 
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
-    has_secure_password
-    validates :password, length: { minimum: 6 }
-  end
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
@@ -18,8 +19,15 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
     def create_remember_token
       self.remember_token = User.digest(User.new_remember_token)
     end
+end
+
